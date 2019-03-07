aea626469ad4c7168d0a0741c9920c5407c52a8e
Eric J. Smith <eric@codesmithtools.com>
2019-03-06 23:42:01 -0600

    Working on new unified messaging abstraction

--- src/Foundatio/Messaging/IMessagePublisher.cs
+++ src/Foundatio/Messaging/IMessagePublisher.cs
@@ -5,12 +5,18 @@ using Foundatio.Queues;
 
 namespace Foundatio.Messaging {
     public interface IMessagePublisher {
-        Task PublishAsync(Type messageType, object message, TimeSpan? delay = null, MessageOptions options = null, CancellationToken cancellationToken = default);
+        // extensions for easily publishing just the raw message body, message settings populated from conventions
+        Task PublishAsync(IMessage message);
     }
 
     public static class MessagePublisherExtensions {
-        public static Task PublishAsync<T>(this IMessagePublisher publisher, T message, TimeSpan? delay = null, MessageOptions options = null) where T : class {
-            return publisher.PublishAsync(typeof(T), message, delay, options);
+        public static Task PublishAsync<T>(this IMessagePublisher publisher, T message) {
+            var m = new Message<T>();
+            return publisher.PublishAsync(m);
+        }
+
+        public static Task PublishAsync<T>(this IMessagePublisher publisher, T message, TimeSpan? delay = null) where T : class {
+            return publisher.PublishAsync(typeof(T), message, delay);
         }
     }
 }
